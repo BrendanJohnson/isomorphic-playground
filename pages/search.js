fe0c84fafc66ccf32b375e11ef019487d778bee1
Brendan Johnson <brendan@quivers.com>
2017-12-08 02:51:53 +1100

    Removed unused code from fetch and server JS and added load on infinite scroll

--- pages/search.js
+++ pages/search.js
@@ -1,42 +1,62 @@
-import React from 'react'
-import { Link } from "../routes";
+import React from "react"
+import { Router } from "../routes"
+import { Link } from "../routes"
 import { get } from "../app/fetch"
-import Property from "../components/property";
+import Property from "../components/property"
 
 export default class Search extends React.Component {
-	static async getInitialProps ({ query: { page = "1", suburb = "Surry", postcode = "2010", tags = "metal", infinite} }) {
-		console.log('render component')
-		console.log(infinite)
+	 constructor (props) {
+	 	console.log('constructor')
+      super(props);
+      this.state = {
+        data: [],
+        page: parseInt(props.page, 10)
+      }
+    }
+	static async getInitialProps ({ query: { page="1", suburb="Surry", postcode="2010", tags="metal" } }) {
 		const hnData = await get(page)
-		//console.log(hnData)
-		
 		return { data: hnData, suburb, page, postcode, tags }
-
 	}
-
 	async componentDidMount() {
+		// Attach a listener to the scroll event on the client side
+		window.addEventListener('scroll', function() {
+        	console.log('scrolling');
+        	const scrollTop = (window.pageYOffset !== undefined)
+        					? window.pageYOffset
+        					: (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
-		console.log();
+        	const documentHeight = Math.max(
+							        document.body.scrollHeight, document.documentElement.scrollHeight,
+							        document.body.offsetHeight, document.documentElement.offsetHeight,
+							        document.body.clientHeight, document.documentElement.clientHeight
+							    );
 
+        	const windowHeight = window.innerHeight||document.documentElement.clientHeight||document.getElementsByTagName('body')[0].clientHeight;
+
+        	if(scrollTop == (documentHeight - windowHeight)) {
+        		const { data, suburb, postcode, tags, page } = this.props
+        		  get(this.state.page + 1).then((response) => {
+				  	this.setState({ page: this.state.page + 1, data: [...this.state.data, ...response] })
+				  })
+        		//Router.pushRoute('search', { page: parseInt(this.state.page, 10) + 1, suburb, postcode, tags }, {shallow: true})
+
+        	}
+
+		}.bind(this));
 
 	}
 	render () {
 		const { data, suburb, postcode, tags, page } = this.props
-		const nextPage = parseInt(page, 10) + 1;
+		
 		return (
 			<main>
-				<h3>Search for properties in {suburb}:</h3>
+				<h3>Search for {tags.replace(/-/g, ', ')} in {suburb.replace(/-/g, ' ')}:</h3>
 				<section>
 					{ data.length
-						? data.map((property, index) => <Property key={property.id} {...property} />)
+						? [...data, ...this.state.data].map((property, index) => <Property key={property.id} {...property} />)
 						: <div>No more properties</div> }
 				</section>
-				<Link route="search" params={{ suburb: suburb, postcode: postcode, tags: tags, page: nextPage, infinite: true }}>
-			          <a>
-			            <span>More</span>
-			          </a>
-			     </Link>
-				<Link prefetch route="search" params={{ suburb: suburb, postcode: postcode, tags: tags, page: nextPage }}>
+				<Link prefetch route="search" params={{ suburb: suburb, postcode: postcode, tags: tags, page: parseInt(page, 10) + 1 }}>
 			          <a>
 			            <span>Next Page</span>
 			          </a>
--- server.js
+++ server.js
@@ -10,26 +10,4 @@ const handler = routes.getRequestHandler(app)
 app.prepare().then(() => {
   const server = express();
   server.use(handler).listen(3000)
-  // // create router from express
-  // const router = express.Router();
-
-  // // this will prevent 404 not found when copy & parse url in browser's address bar
-  // router.get('/:slug', (req, res) => {
-  //   return app.render(req, res, '/search', req.query);
-  // });
-
-  // router.get('*', (req, res) => {
-  //   handle(req, res);
-  // });
-
-  // // bind prefix to resource url
-  // server.use('/prefix', router); 
-  // server.use('/prefix/static', express.static('static'));
-  // server.use(handle);
-
-  // server.listen(3000, err => {
-  //   if (err)
-  //     throw err;
-  //   console.log(`> Ready on http://localhost:3000/prefix`);
-  // });
 });
